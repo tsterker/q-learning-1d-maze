01435592dfbf8565a7958c49c798cf2fd4146b47
Tim Sterker <t.sterker@gmail.com>
2014-02-02 15:35:37 +0000

    initial commit

---
+++ js/QLearner.js
@@ -0,0 +1,193 @@
+
+;(function(LearnTool){
+
+    // =======================================================================
+    // # Private
+    // =======================================================================
+    var states = {
+        GOAL: 1
+    };
+
+    var actions = {
+        LEFT: -1,
+        RIGHT: +1
+    }
+
+    var Q = {
+        data: {},
+        reset: function(){
+            this.data = {};
+        },
+        update: function(s, a, value){
+            this.data[[s, a]] = value;
+        },
+        get: function(s, a){
+            var value = this.data[[s, a]];
+            return value? value : 0;
+        }
+    };
+
+    function V(s){
+        var a, r;
+        var max = -Infinity;
+        for(var name in actions){
+            r = Q.get(s, actions[name])
+            if(max < r){
+                max = r;
+            }
+        }
+        return max;
+    }
+
+    function argmaxQ_a(s){
+        var a, r;
+        var max = -Infinity;
+        var argmax = 0;
+        for(var name in actions){
+            r = Q.get(s, actions[name])
+            if(max < r){
+                max = r;
+                argmax = actions[name];
+            }
+        }
+        return argmax;
+    }
+
+
+    function randomState(N){
+        return Math.round(Math.random()*(N-1)) + 1;
+    }
+
+    function randomAction(){
+        return (Math.random() < 0.5)? -1 : 1;
+    }
+
+
+    // =======================================================================
+    // # Class
+    // =======================================================================
+    var QLearner = {
+        //initialized: false,
+
+        N: 5,
+        gamma: 0.99,
+        epsilon: 1,
+        state: undefined,
+        action: undefined,
+
+        showQ: function(){
+            console.log(Q.data);
+        },
+        getQ: function(){
+            return Q.data;
+        },
+        reset: function(){
+            this.state = randomState(this.N);
+            Q.reset();
+        },
+
+        runEpisodes: function(options, active){
+            options = options || {};
+            options.episodes = Math.round(options.episodes);
+            options.episodes = isNaN(options.episodes)? this.N*this.N : options.episodes;
+            options = options || {};
+            var self = this;
+
+            if(options.episodes <= 0){
+                console.log("!!!!!!!!!!!!!!!! DONE !!!!!!!!!!!!!!!!!!");
+                return;
+            }
+            options.episodes -= 1;
+
+            if(!active){
+                _whenDone = options.whenDone;
+                options.whenDone = function(){
+                    console.log("END ", options.episodes, 'episode');
+                    self.runEpisodes(options, true);
+                    _whenDone && _whenDone();
+                };
+            }
+            self.runEpisode(options);
+        },
+        runEpisode: function(options, active){
+            // new random state if episode newly started
+            if(!active){
+                this.state = randomState(this.N);
+            }
+            var self = this;
+            var success = false;
+            options = options || {};
+
+            setTimeout(function(){
+                success = self.step();
+                if(!success){
+                    options.eachStep && options.eachStep();
+                    self.runEpisode(options, true);
+                } else {
+                    options.whenDone && options.whenDone();
+                }
+            }, options.delay || 10);
+        },
+        step: function(){
+
+            var nextState;
+            this.prevState = this.state;
+            var maxQValue = 0;
+
+            var takeGreedyAction = (Math.random() <= this.epsilon);
+            console.log(takeGreedyAction);
+
+            if(this.greedy){
+                // e-Greedy action; e = 0
+                // at = arg maxaQ (st , a)
+                this.action = argmaxQ_a(this.state);
+                console.log(this.action);
+            } else {
+                // Exlploration:
+                // select one among all possible actions for the current state
+                this.action = randomAction();
+            }
+
+
+            nextState = this.state + this.action;
+
+            // using this possible action, consider going to the next state
+            // -> get maximum Q value for this next state based on all possible actions
+            if(nextState > 1){
+                //maxQValue = 0;              // all possible states have 0 reward
+                //maxQValue = Q.get(nextState, this.action);
+                maxQValue = V(nextState);
+
+            } else if(nextState === 1) {
+                maxQValue = 1;
+            }
+            Q.update(this.state, this.action, this.gamma * maxQValue);
+
+            // -> set next state as current state
+            this.state = nextState;
+
+            // prevent running out to the right
+            if(this.state > this.N){
+                this.state = this.prevState;
+            }
+            if(this.state <= 0){
+                this.state = this.prevState;
+            }
+
+
+
+            if(this.state === states.GOAL){
+                //this.state = randomState(this.N);
+                return true;
+            }
+        }
+    };
+    QLearner.reset();
+
+    // =======================================================================
+    // # Public
+    // =======================================================================
+
+    LearnTool.QLearner = QLearner;
+
+}(window.LearnTool = window.LearnTool || {}))
---
+++ js/main.js
@@ -0,0 +1,132 @@
+
+
+// ? function reward(s){
+// ?     return (s === 1)? 1 : 0;
+// ? }
+
+
+var can = document.getElementById('canvas');
+var ctx = can.getContext('2d');
+
+
+function drawStates(c, N, currentState, goalState){
+    var w = c.canvas.width;
+    var h = c.canvas.height;
+    var stateSize = w/N;
+    var stateDist = w/N * 0.5;
+    stateSize = stateSize - (stateSize - stateDist);
+
+    for(var i = 1; i <= N; i++){
+        c.strokeStyle = 'transparent';
+        c.fillStyle = 'transparent';
+        if(i === currentState){
+            c.fillStyle = 'green';
+        } else if(i === goalState) {
+            c.strokeStyle = 'red';
+        } else {
+            c.strokeStyle = 'black';
+        }
+
+        var x = stateDist/2 + (i-1)*stateSize + (i-1)*stateDist;
+        var y = h/2 - stateSize/2;
+
+        c.fillRect(x, y, stateSize, stateSize);
+        c.strokeRect(x, y, stateSize, stateSize);
+
+        if(i === currentState){
+            c.strokeStyle = 'green';
+            c.beginPath();
+            //c.moveTo();
+            //c.lineTo();
+            c.stroke();
+        }
+
+        c.fillStyle = 'black';
+        c.textAlign = 'left';
+        c.textBaseline = 'top';
+        c.font = 'bold 12px sans-serif'
+        c.fillText(i, x, h/2 + stateSize/2 + 5);
+
+    }
+
+}
+
+function debug() {
+    var out = document.getElementById('debug');
+    //out.innerHTML = JSON.stringify(Q);
+    var tmpl_qTable = _.template(document.getElementById('q-table').innerHTML);
+    out.innerHTML = tmpl_qTable({
+        N: learner.N,
+        Q: learner.getQ()
+    });
+
+//    out.innerHTML = '';
+//    out.innerHTML += '.| left | right';
+//    out.innerHTML += '<br>';
+//    out.innerHTML += '================';
+//    out.innerHTML += '<br>';
+//    for(var i = 0; i < learner.N; i++){
+//        out.innerHTML += i + '|';
+//        out.innerHTML += '<br>';
+//        out.innerHTML += '.|--------------';
+//        out.innerHTML += '<br>';
+//    }
+
+}
+
+function redraw(){
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'black';
+    ctx.fillRect(0, 0, can.width, can.height);
+    ctx.strokeRect(0, 0, can.width, can.height);
+    drawStates(ctx, learner.N, learner.state, 1);
+}
+
+function update() {
+    redraw(ctx);
+    debug();
+}
+
+function runQLearnerExploration(times) {
+    learner.greedy = false;
+    runQLearner(times);
+}
+function runQLearnerGreedy(times) {
+    learner.greedy = true;
+    runQLearner(times);
+}
+
+function runQLearner(times) {
+    //learner.runEpisode(times || learner.N * learner.N, function(){
+    learner.runEpisodes({
+        episodes: 5,
+        //delay: 200,
+        delay: 10,
+        eachStep: function(){
+            //console.log('step', learner.state)
+            console.log(learner.prevState, '-->', learner.state);
+            update();
+        },
+        whenDone: function(){
+            console.log('[ ===== GOAL ===== ]');
+            update();
+        }
+    });
+    update();
+}
+
+
+
+
+// Setup
+// ==================================================
+
+var learner = LearnTool.QLearner;
+learner.N = 20;
+learner.gamma = 0.8;
+
+// Visualization
+// ==================================================
+update();
+
+
